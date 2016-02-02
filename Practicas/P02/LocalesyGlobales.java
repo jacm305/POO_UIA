6e0671fca1c687eae62f24639b0dfd280d80aa56
jacm305 <jacm306@gmail.com>
2016-02-02 03:33:17 -0600

    Estructura del repositorio

---
+++ Practicas/P02/LocalesyGlobales.java
@@ -0,0 +1,13 @@
+public class LocalesyGlobales  {
+	public static String global = "variable global";
+
+	public static void main(String[] args) {
+
+		String local = "variable local";
+		
+		System.out.println("Tipos de Variables:");
+		System.out.println(global);
+		System.out.println(local);
+		
+}
+}
---
+++ Practicas/P02/TiposdeDatos.java
@@ -0,0 +1,34 @@
+public class TiposdeDatos  {
+	public static void main(String[] args) {
+//Declaración de la variable char;
+char michar = 'c'; //Los char se ponen entre comilla simple ' '
+
+//Declaración de tipo booleano
+boolean miboleano = true;
+
+//Declaración de tipo byte
+byte mibyte = 58; //No se debe exceder de 128
+
+//Declaración de tipo short
+short mishort = 500;
+
+//Declaración de tipo int
+int miint = 245474;
+
+//Declaración de tipo float
+float mifloat = 2436.2456f; //se pone una f para decir que es float
+
+//Declaración de tipo double
+double midouble = 2436.2456;
+
+		System.out.println("TiposdeDatos:");
+		System.out.println(michar);
+		System.out.println(mibyte);
+		System.out.println(miboleano);
+		System.out.println(mibyte);
+		System.out.println(mishort);
+		System.out.println(miint);
+		System.out.println(mifloat);
+		System.out.println(midouble);
+}
+}
