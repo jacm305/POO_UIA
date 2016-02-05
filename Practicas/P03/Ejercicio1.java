e0a5e8b96d546626bd5f7c138825e274a5daff1f
jacm305 <jacm306@gmail.com>
2016-02-04 18:49:14 -0600

    Estructura del repositorio

---
+++ Practicas/P03/Ejercicio1.java
@@ -0,0 +1,24 @@
+public class Ejercicio1
+{
+	public static void main (String [] args)
+	{
+		int a = 10, b = 3, c = 1, d, e;
+		int x, y;
+		Boolean c1;
+		//a++;
+		//b++;
+
+		
+		x = a / b;
+
+		System.out.println("El resultado es;" + x);
+
+		c1 = a < b && c < a;
+
+		System.out.println("El resultado es:" + c1);
+
+		//int d = a + b++;
+		//int e = ++a - b;
+		//Float y = (Float) a/b;
+	}
+}
---
+++ Practicas/P03/MiAplicacion.java
@@ -0,0 +1,13 @@
+public class MiAplicacion
+{
+	public static void main (String [] args)
+	{
+		double c3 = 1, c2 = 2, c1 =3, c0 = 4;
+		double x = 2;
+		double resultado;
+
+		resultado = c3*x*x*x + c2*x*x + c1*x + c0;
+
+		System.out.println(resultado);
+	}
+}
